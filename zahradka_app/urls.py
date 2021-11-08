b65af8a771d7010f788edf939974cc2427b14018
Vit Kuntos <v.kuntos@gmail.com>
2021-11-08 15:49:40 +0100

    login, register

--- zahradka_app/urls.py
+++ zahradka_app/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from .views import RegistrationView, LoginView, LogoutView, ChangePasswordView
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.homepage, name="homepage"),
+    path("register/", RegistrationView.as_view(), name="register"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("change_password/", ChangePasswordView.as_view(), name="change_password"),
 ]
--- zahradka_app/views.py
+++ zahradka_app/views.py
@@ -1,4 +1,56 @@
-from django.shortcuts import render, HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import PasswordChangeView
+from django.shortcuts import render, HttpResponse, resolve_url, redirect
+from django.template.response import TemplateResponse
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import TemplateView
+from django.views.generic.edit import FormMixin
 
-def index(request):
-    return HttpResponse("Ahoj světe!")
+from zahradka_app.forms import RegistrationForm
+
+
+def homepage(request):
+    return render(request, "homepage.html")
+
+class LogoutView(View):
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        return redirect("homepage")
+
+
+class LoginView(FormMixin, TemplateView):
+    template_name = "accounts/login.html"
+    form_class = AuthenticationForm
+
+    def post(self, request, *args, **kwargs):
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+
+        if user is None:
+            # TODO add messages
+            return redirect("login")
+
+        login(request, user)
+        return redirect("homepage")
+
+
+class ChangePasswordView(PasswordChangeView):
+    template_name = "accounts/change_password.html"
+    success_url = reverse_lazy("homepage")
+
+
+class RegistrationView(FormMixin, TemplateView):
+    template_name = "accounts/register.html"
+    form_class = RegistrationForm
+
+    def post(self, request, *args, **kwargs):
+        bounded_form = self.form_class(request.POST)
+        if bounded_form.is_valid():
+            bounded_form.save()
+            return redirect("homepage")
+        else:
+            return TemplateResponse(request, "accounts/register.html", context={"form": bounded_form})
+
